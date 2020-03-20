3ea2edb669359f4de06b9762615d0b4ace7dee9c
Ayan Chatterjee <ayanchatterjeebgkv@gmail.com>
2020-03-20 21:00:28 +0530

    sad

--- basic.cpp
+++ basic.cpp
@@ -1,29 +1,38 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
-    
-    int vertices,edges,u,v; //u and v are connected edges
-    cout<<"enter the number of vertices and edges"<<endl;
-    cin>>vertices>>edges;
-    
-    vector<int> adj[100];
- 
-    cout<<"enter enter the edges(u and v)"<<endl;
-    for(int i=0;i<edges;i++){
-        cin>>u>>v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
+
+void addEdge(vector<int> adj[],int ver,int edgeTo){
+
+    adj[ver].push_back(edgeTo);
+    adj[edgeTo].push_back(ver);
+}
 
 
-    for(int i=0;i<vertices;i++){
-        cout<<i;
-        for(auto it:adj[i]){
-            cout<<"->"<<it;
+void printGraph(vector<int> adj[],int edgeLimit){
+    int j;
+    for(int i=0;i<edgeLimit;i++){
+        cout<<"adjacency list for "<<i<<" :";
+        for(auto j:adj[i]){
+            cout<<"->"<<j;
         }
         cout<<endl;
     }
-    
+}
+void makeGraph(){
+    int edgeLimit,ver,edgeTo;
+    cout<<"enter the number of edges"<<endl;
+    cin>>edgeLimit;
+      vector<int> adj[100];
+    cout<<"now, enter the vertex, 'vertex to which it is connected' pair"<<endl;
+
+    for(int i=0;i<edgeLimit;i++){
+        cin>>ver>>edgeTo;
+        addEdge(adj,ver,edgeTo);
+    }
+    printGraph(adj,edgeLimit);
+}
+int main(){
+    makeGraph();
     return 0;
 }
