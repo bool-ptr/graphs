905682884a85e39c1dafd39e01408eb1576e4d73
Ayan Chatterjee <ayanchatterjeebgkv@gmail.com>
2020-03-23 16:21:01 +0530

    DFS using lists

--- DFS.cpp
+++ DFS.cpp
@@ -1,41 +1,55 @@
-#include<iostream>
+#include <iostream>
+#include <list>
+#include <iterator>
 using namespace std;
-int cost[10][10],i,j,k,n,stk[10],top,v,visit[10],visited[10];
+ 
+class Graph
+{
+    int numVertices;
+    list<int> *adjLists;
+    bool *visited;
+    
+public:
+    Graph(int V);
+    void addEdge(int src, int dest);
+    void DFS(int vertex);
+};
+ 
+Graph::Graph(int vertices)
+{
+    numVertices = vertices;
+    adjLists = new list<int>[vertices];
+    visited = new bool[vertices];
+}
+ 
+void Graph::addEdge(int src, int dest)
+{
+    adjLists[src].push_front(dest);
+}
+ 
+void Graph::DFS(int vertex)
+{
+
+    visited[vertex] = true;
+    list<int> adjList = adjLists[vertex];
+    
+    cout << vertex << endl;
+ 
+    list <int>::iterator i;
+    for(i = adjList.begin(); i != adjList.end(); ++i)
+        if(!visited[*i])
+            DFS(*i);
+}
+ 
 int main()
 {
-    int m;
-   
-    cout <<"Enter no of vertices:";
-    cin >> n;
-    cout <<"Enter no of edges:";
-    cin >> m;
-    cout <<"\nedges \n";
-    for(k=1; k<=m; k++)
-    {
-        cin >>i>>j;
-        cost[i][j]=1;
-    }
-    cout <<"Enter initial vertex to traverse from:";
-    cin >>v;
-    cout <<"DFS ORDER OF VISITED VERTICES:";
-    cout << v <<" ";
-    visited[v]=1;
-    k=1;
-    while(k<n)
-    {
-        for(j=n; j>=1; j--)
-            if(cost[v][j]!=0 && visited[j]!=1 && visit[j]!=1)
-            {
-                visit[j]=1;
-                stk[top]=j;
-                top++;
-            }
-        v=stk[--top];
-        cout<<v << " ";
-        k++;
-        visit[v]=0;
-        visited[v]=1;
-    }
-   
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+
+    g.DFS(2);
+ 
     return 0;
 }
